30e45de6a7ae95cc43f18612e70414128feea4a8
Charleen D. Adams <charleen.adams@bristol.ac.uk>
2017-02-22 15:22:05 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
 ui = basicPage(
-  h2('ProtecT Results to Assess the Causal Effect of Metabolites on Prostate Cancer'),
+  h2('ProtecT Results Associating Metabolites with Odds of Prostate Cancer Incidence'),
   dataTableOutput('mytable')
 )
